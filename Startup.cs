13c74e0ee9b8d1f58597ae51bfbcc7bedb4c2e00
Henry <henrichukwudi@gmail.com>
2022-06-26 19:49:27 +0100

    updated some views

--- Startup.cs
+++ Startup.cs
@@ -73,7 +73,7 @@ namespace Expense_Tracker
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Expense}/{action=Index}/{id?}");
             });
         }
     }
